8a4d0bd02009aedeccae26e611b5a60252fa300d
Enes Aksoy <enesaksoy1168@gmail.com>
2021-04-24 06:02:02 +0300

    CLAuthorizationStatus added on HomeScreenViewController.

--- Kafein/Screen/HomeScreen/ViewController/HomeScreenViewController.swift
+++ Kafein/Screen/HomeScreen/ViewController/HomeScreenViewController.swift
@@ -6,13 +6,29 @@
 //
 
 import UIKit
+import CoreLocation
 
 class HomeScreenViewController: UIViewController {
+    
+    // MARK: - Proporties
+    
+    let locationManager = CLLocationManager()
+    
+    // MARK: - Life Cycles
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+        self.locationManager.requestWhenInUseAuthorization()
+        self.locationManager.delegate = self
     }
-    
+}
+
+    // MARK: - CLLocationManagerDelegate Methods
+
+extension HomeScreenViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedAlways || status == .authorizedWhenInUse {
+            // call api
+        }
+    }
 }
