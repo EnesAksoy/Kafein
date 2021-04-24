3f69b758968dc99d54737ec67b6a68f2e6e396b7
Enes Aksoy <enesaksoy1168@gmail.com>
2021-04-24 04:58:45 +0300

    SplashScreen created.

---
+++ Kafein/Screen/SplashScreen/ViewController/SplashScreenViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SplashScreenViewController.swift
+//  Kafein
+//
+//  Created by ENES AKSOY on 24.04.2021.
+//
+
+import UIKit
+
+class SplashScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
