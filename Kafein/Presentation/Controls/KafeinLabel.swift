7575207d8fbe0d764651987813b381291d9db1bc
Enes Aksoy <enesaksoy1168@gmail.com>
2021-04-24 20:50:37 +0300

    Custom Kafein Label added.

---
+++ Kafein/Presentation/Controls/KafeinLabel.swift
@@ -0,0 +1,20 @@
+//
+//  KafeinLabel.swift
+//  Kafein
+//
+//  Created by ENES AKSOY on 24.04.2021.
+//
+
+import Foundation
+
+import UIKit
+
+class KafeinLabel: UILabel {
+    @IBInspectable var localizableKey: String? = nil {
+        didSet {
+            if localizableKey != nil {
+                self.text = NSLocalizedString(localizableKey!, comment: "")
+            }
+        }
+    }
+}
